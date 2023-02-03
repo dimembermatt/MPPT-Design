46c0097f70641185e9e6b0f24b447f2486bd9e49
Matthew Yu <matthewjkyu@gmail.com>
2023-02-03 01:01:56 -0600

    fix bugs and standard inputs/output

--- sw/design.py
+++ sw/design.py
@@ -85,22 +85,26 @@ if __name__ == "__main__":
     except ImportError:
         pass  # no need to fail because of missing dev dependency
 
+    plt.ion()
+
     # Given a set of inputs
     num_cells = 111
     v_oc = 0.721
     i_sc = 6.15
     v_mpp = 0.621
     i_mpp = 5.84
+    # Note that we get divide by 0 errors should the lower bounds be 0% or 100%
+    # of the array voltage. DON'T DO IT!
     v_in_range = [num_cells * v_oc * 0.05, num_cells * v_oc * 0.95]  # V
     v_in_opt = num_cells * v_mpp  # V
     v_out_range = [80, 134.4]  # V
     i_in_range = [0, i_sc]  # A
 
     # And a specified efficiency and ripple
-    eff = 0.99
-    r_ci_v = 0.5  # V
-    r_co_v = 0.5  # V
-    r_l_a = 0.1  # A
+    eff = 0.975
+    r_ci_v = 0.1  # V
+    r_co_v = 0.1  # V
+    r_l_a = 0.05  # A
     r_ci = r_ci_v / v_in_range[1]
     r_co = r_co_v / v_out_range[1]
     r_l = r_l_a / i_in_range[1]
@@ -124,6 +128,43 @@ if __name__ == "__main__":
     print(f"    R_L - {r_l_a} A [{r_l*100} %]")
     print(f"Safety Factor: {sf}")
 
+    # Step A. Plot duty cycle ratio for input/output combos.
+    print(f"----------------------------------------")
+    print(f"STEP A")
+    print(
+        f"Generating duty cycle surface map."
+    )
+
+    x_v_in = []
+    y_v_out = []
+    z_duty = []
+    for v_in in np.linspace(v_in_range[0], v_in_range[1], num=50, endpoint=True):
+        for v_out in np.linspace(v_out_range[0], v_out_range[1], num=50, endpoint=True):
+            duty = 1 - v_in / v_out
+            x_v_in.append(v_in)
+            y_v_out.append(v_out)
+            z_duty.append(duty)
+
+    # Plot out switching frequency map.
+    ax_duty = fig.add_subplot(projection="3d")
+    ax_duty.scatter(x_v_in, y_v_out, z_duty, c=z_duty)
+    ax_duty.set_title("Minimum Duty Cycle Across I/O Mapping")
+    ax_duty.set_xlabel("V_IN (V)")
+    ax_duty.set_ylabel("V_OUT (V)")
+    ax_duty.set_zlabel("Duty Cycle")
+
+    print(
+        "Note that if the next step fails, R_DS_ON is probably too high. "
+        "The following actions can be performed:"
+        "\n\t- tradeoff r_ds_on for higher c_oss (you'll probably find yourself here)"
+        "\n\t- increase min v_in relative to v_out to reduce max duty (a reasonable option)"
+        "\n\t- decrease max v_out relative to v_in to reduce max duty (a less reasonable option)"
+        "\n\t- decrease design efficiency (undesirable for MPPT, last resort)"
+    )
+
+    plt.show()
+
+
     # Step 1. Derive the initial switch requirements and choose a switch.
     # Minimize the FOM, which is beyond the scope of this script.
     print(f"----------------------------------------")
@@ -145,13 +186,14 @@ if __name__ == "__main__":
     c_oss = float(input("C_OSS (pF): ")) * 10**-12
     r_ds_on = float(input("R_DS_ON (mOhm): ")) * 10**-3
 
+
     # Step 2. After selecting the switch, calculate the FOM. We then determine
     # the maximum switching frequency across the input/output map that meets our
     # efficiency metric.
     print(f"----------------------------------------")
     print(f"STEP 2")
     tau = c_oss * r_ds_on
-    print(f"FOM for this switch: {tau * 10**9} (ns).")
+    print(f"FOM for this switch: {tau * 10**12} (ps).")
     print(
         f"Generating f_s_max surface map complying w/ device efficiency "
         f"({v_in_opt * i_mpp * (1 - eff)} W max dissipation)."
@@ -161,8 +203,8 @@ if __name__ == "__main__":
     y_v_out = []
     z_f_s = []
 
-    v_in_combos = np.linspace(v_in_range[0], v_in_range[1], num=100, endpoint=False)
-    v_out_combos = np.linspace(v_out_range[0], v_out_range[1], num=100, endpoint=False)
+    v_in_combos = np.linspace(v_in_range[0], v_in_range[1], num=50, endpoint=True)
+    v_out_combos = np.linspace(v_out_range[0], v_out_range[1], num=50, endpoint=True)
     for v_in in v_in_combos:
         i_in = model_nonideal_cell(1000, 298.15, 0, 100, v_in / num_cells)
         for v_out in v_out_combos:
@@ -173,27 +215,29 @@ if __name__ == "__main__":
                 loss_con, loss_swi, loss_tot = get_losses(
                     v_in, i_in, v_out, f_s, r_ds_on, c_oss, r_l
                 )
-                if loss_tot > v_in_opt * i_mpp * (1 - eff):
+                if loss_tot >= v_in_opt * i_mpp * (1 - eff):
                     break
-                f_s *= 1.1
+                f_s *= 1.025
 
             x_v_in.append(v_in)
             y_v_out.append(v_out)
-            z_f_s.append(f_s)
+            z_f_s.append(f_s * 10**-3)
+
+    plt.clf()
 
     # Plot out switching frequency map.
-    ax = fig.add_subplot(projection="3d")
-    ax.scatter(x_v_in, y_v_out, z_f_s, c=z_f_s)
-    ax.set_title("Max Frequency within Power Budget Across I/O Mapping")
-    ax.set_xlabel("V_IN (V)")
-    ax.set_ylabel("V_OUT (V)")
-    ax.set_zlabel("F_S_MAX (Hz)")
+    ax_freq = fig.add_subplot(projection="3d")
+    ax_freq.scatter(x_v_in, y_v_out, z_f_s, c=z_f_s)
+    ax_freq.set_title("Max Frequency within Power Budget Across I/O Mapping")
+    ax_freq.set_xlabel("V_IN (V)")
+    ax_freq.set_ylabel("V_OUT (V)")
+    ax_freq.set_zlabel("F_S_MAX (kHz)")
 
     plt.show()
 
     # Determine minimum frequency allowed across the map.
     print(
-        f"Minimum frequency for the converter: {math.floor(min(z_f_s)) * 10**-3} kHz "
+        f"Minimum frequency for the converter: {math.floor(min(z_f_s))} kHz "
         "(choosing lower is acceptable but will result in larger ripple.)"
     )
 
@@ -206,13 +250,15 @@ if __name__ == "__main__":
     print(f"STEP 3")
 
     # Given that the PV is a nonlinear current source, it's not directly
-    # straightforward to determine when ci, co, l are minimized.
+    # straightforward to determine when ci, co, l are minimized. We do know,
+    # however, that the minimum feasible passive value is bounded by the worst
+    # input/output combo.
     ci_min = []
     co_min = []
     l_min = []
 
-    v_in_combos = np.linspace(v_in_range[0], v_in_range[1], num=100, endpoint=False)
-    v_out_combos = np.linspace(v_out_range[0], v_out_range[1], num=100, endpoint=False)
+    v_in_combos = np.linspace(v_in_range[0], v_in_range[1], num=50, endpoint=True)
+    v_out_combos = np.linspace(v_out_range[0], v_out_range[1], num=50, endpoint=True)
     for v_in in v_in_combos:
         i_in = model_nonideal_cell(1000, 298.15, 0, 100, v_in / num_cells)
         p_in = v_in * i_in
@@ -221,10 +267,11 @@ if __name__ == "__main__":
             co_min.append((v_out - v_in) * p_in / (2 * r_co * v_out**3 * f_s))
             l_min.append((v_in - v_in**2 / v_out) / (2 * i_in * f_s * r_l))
 
-    print(f"Minimum C_IN: {np.min(ci_min) * 10**6} uF")
-    print(f"Minimum C_OUT: {np.min(co_min) * 10**6} uF")
-    print(f"Minimum L: {np.min(l_min) * 10**6} uH")
+    print(f"Minimum C_IN: {np.max(ci_min) * 10**6} uF")
+    print(f"Minimum C_OUT: {np.max(co_min) * 10**6} uF")
+    print(f"Minimum L: {np.max(l_min) * 10**6} uH")
     print(
         f"Capacitors and inductors should be rated for {v_out_range[1] * sf} V and {i_mpp * sf} A."
     )
 
+    input("Press any key to end.")
