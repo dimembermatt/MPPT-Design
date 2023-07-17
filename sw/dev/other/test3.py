cca0876a84ea211fb249128594a138e7f8b16cdb
Matthew Yu <matthewjkyu@gmail.com>
2023-07-17 18:18:58 -0500

    rm dev folder which breaks linux kids

--- sw/dev/other/test3.py
+++
@@ -1,9 +0,0 @@
-L = 310 * 10**-6  # H
-I_max = 9  # A
-rho = 1.77 * 10**-8  # ohm meters
-B_max = 530 * 10**-3  # T
-R_max = 70 * 10**-3  # ohm
-k_u = 0.3
-
-kg = ((L**2) * (I_max**2) * rho) / ((B_max * 0.75) ** 2 * R_max * k_u)
-print(kg * 1e10)
